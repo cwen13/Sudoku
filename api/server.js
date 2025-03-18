ff92579e4fd119935d824e03758c801e79a1a8d2
cwen13 <cwenrich13@gmail.com>
2025-03-18 02:30:05 -0400

    update server to work wth vercel

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
