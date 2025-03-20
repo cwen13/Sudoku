8c02a163347d0a8d75d26f6259c49d7071998d63
cwen13 <cwenrich13@gmail.com>
2025-03-20 02:20:45 -0400

    starting reactive styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,38 @@ import { GridContextProvider } from "./utils/GridContext";
 
 function App() {
 
+  const [mobile, setMobile] = useState(
+    window.matchMedia("(min-widdth: 350px)").matches
+  }
 
-  
+  useEffect(() => {
+    window
+      .matchMedia("(min-widdth: 350px)")
+      .addEventListener('change', e => setMatches(e.matches));
+  },[]);
+ 
   return (
     <>
       <GridContextProvider>
       <header>
 	<p>REACTJS:  Sudoku</p>
       </header>
-      <section className="main-page">
-	<Grid id="grid" />
-	<Sidebar id="sidebar" />
-      </section>
-
-      <footer>
-	<div>
-	  <p>Made with React and Vite by Cody Wenrich-Clegg</p>
-	  <p>
-	    
-	    Source code <a href="https://github.com/cwen13/Sudoku"
-			    target="_blank"
-			>here</a>
-	  </p>
+	
+	<section className="main-page">
+	  <Grid id="grid" />
+	  <Sidebar id="sidebar" />
+	</section>
+	
+	<footer>
+	  <div>
+	    <p>Made with React and Vite by Cody Wenrich-Clegg</p>
+	    <p>	    
+	      Source code <a href="https://github.com/cwen13/Sudoku"
+			     target="_blank"
+			  >here</a>
+	    </p>
 	  </div>
-      </footer>
-
+	</footer>
       </GridContextProvider>
     </>
   )
